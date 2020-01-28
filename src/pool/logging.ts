daaf18e72ec859b8da30d9d6ddb5ff4e5e2f1808
Razvan Marinovici <razvan.marinovici@codefysoftware.com>
2020-01-28 17:58:50 +0200

    Minor cleanup.

--- src/pool/logging.ts
+++ src/pool/logging.ts
@@ -3,9 +3,9 @@ import * as MY_SPECIAL_WORKER_KEY from 'worker_threads';
 
 export type Logger = (msg: string, ...param: any[]) => void;
 
+const _rootLog = util.debuglog('lib-workers');
 export function getLog(ctx: string): Logger {
-  const log = util.debuglog('lib-workers');
   return (msg: string, ...param: any[]) => {
-    log(`[${Date.now()}] [W-${MY_SPECIAL_WORKER_KEY.threadId}] ${ctx} - ${msg}`, param);
+    _rootLog(`[${Date.now()}] [W-${MY_SPECIAL_WORKER_KEY.threadId}] ${ctx} - ${msg}`, param);
   };
 }
