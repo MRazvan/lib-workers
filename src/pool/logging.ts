725ccaa80a00ea80c256dd02bee0c4fd22076764
Razvan Marinovici <razvan.marinovici@codefysoftware.com>
2020-01-29 12:09:30 +0200

    Better time resolution for log

--- src/pool/logging.ts
+++ src/pool/logging.ts
@@ -1,11 +1,14 @@
 import * as util from 'util';
 import * as MY_SPECIAL_WORKER_KEY from 'worker_threads';
+const _rootLog = util.debuglog('lib-workers');
+function getHRTime(): number {
+  const time = process.hrtime();
+  return time[0] * 10e9 + time[1];
+}
 
 export type Logger = (msg: string, ...param: any[]) => void;
-
-const _rootLog = util.debuglog('lib-workers');
 export function getLog(ctx: string): Logger {
   return (msg: string, ...param: any[]) => {
-    _rootLog(`[${Date.now()}] [W-${MY_SPECIAL_WORKER_KEY.threadId}] ${ctx} - ${msg}`, param);
+    _rootLog(`[${getHRTime()}] [W-${MY_SPECIAL_WORKER_KEY.threadId}] ${ctx} - ${msg}`, param);
   };
 }
