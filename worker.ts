1994205e66c35f92cdad220375503269394d0bd4
Razvan Marinovici <razvan.marinovici@codefysoftware.com>
2020-01-30 12:46:04 +0200

    Cleanup.
    File not needed.

--- worker.ts
+++
@@ -1,8 +0,0 @@
-import * as WP from './src/pool';
-
-@WP.ThreadLoad()
-export class Test {
-  public resu(): Promise<string> {
-    return Promise.resolve('Hello World ' + WP.Threading.threadId);
-  }
-}
