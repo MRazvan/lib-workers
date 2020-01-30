0b1867474cbfbe3c840c13ba2ebbde55e7a7cd54
Razvan Marinovici <razvan.marinovici@codefysoftware.com>
2020-01-30 11:52:58 +0200

    Revert logging timestamp
    We don't need nanosecond precision when logging, we need a timestamp to keep everything in order and that is it. The nanosecond precision will be usefull in tracing.

--- src/pool/logging.ts
+++ src/pool/logging.ts
@@ -1,14 +1,9 @@
 import * as util from 'util';
 import * as MY_SPECIAL_WORKER_KEY from 'worker_threads';
 const _rootLog = util.debuglog('lib-workers');
-function getHRTime(): number {
-  const time = process.hrtime();
-  return time[0] * 10e9 + time[1];
-}
-
 export type Logger = (msg: string, ...param: any[]) => void;
 export function getLog(ctx: string): Logger {
   return (msg: string, ...param: any[]) => {
-    _rootLog(`[${getHRTime()}] [W-${MY_SPECIAL_WORKER_KEY.threadId}] ${ctx} - ${msg}`, param);
+    _rootLog(`[${Date.now()}] [W-${MY_SPECIAL_WORKER_KEY.threadId}] ${ctx} - ${msg}`, param);
   };
 }
